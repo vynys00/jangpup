8a5297c8f5cf93fb089fd32c1ee62a8f1a25df8a
Vynys <erwinpben2@gmail.com>
2024-07-12 23:00:19 +0800

    test

--- instagram.js
+++ instagram.js
@@ -27,6 +27,7 @@ async function downloadInstagramMedia(url, message) {
       viewport: { width: 412, height: 915 },
       userAgent:
         "Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G973U) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36",
+      video: "on",
     });
 
     // Navigate to the Instagram URL
